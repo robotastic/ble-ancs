1a084161994d4fa2ada3e14352cd1d9420f31f01
robotastic <luke@robotastic.com>
2015-10-15 21:56:12 -0400

    test with gpi0

--- test-gpio.js
+++ test-gpio.js
@@ -7,18 +7,20 @@ ancs.on('notification', function(notification) {
 
 //will blink an led on GPIO pin 7 everytime there is a new notification
 
-notification.readTitle( function(title) {
-	notification.readMessage( function(message) {
-		console.log("Notification: " + notification);
-		gpio.open(7, "output", function(err) {     // Open pin 16 for output 
+	notification.readTitle( function(title) {
+		notification.readMessage( function(message) {
+			console.log("Notification: " + notification);
+			
+		});
+	});
+	gpio.open(7, "output", function(err) {     // Open pin 16 for output 
    		gpio.write(7, 1, function() {          // Set pin 16 high (1) 
    			setTimeout(function() {
    				gpio.write(7, 1, function() { 
-        			gpio.close(16);  
+        			gpio.close(7);  
         		});                   // Close pin 16 
     		},1000);
 		});
 	});
-});
 
 });
